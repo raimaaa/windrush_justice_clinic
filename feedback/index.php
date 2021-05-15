9ee5b7169408ccadd2c1fa879e154562e44238bc
Mholomah <82943778+Mholomah@users.noreply.github.com>
2021-05-15 13:29:09 +0100

    Update index.php

--- feedback/index.php
+++ feedback/index.php
@@ -1,7 +1,4 @@
-<!--Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/-->
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,204 +14,140 @@ License URL: http://creativecommons.org/licenses/by/3.0/-->
 <link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 </head>
 <body class="agileits_w3layouts">
-    <h1 class="agile_head text-center">Windrush Justice Clinic</h1>
+    <h1 class="agile_head text-center">Windrush Scheme Application</h1>
     <div class="w3layouts_main wrap">
-
-			<!-- _____________________ INITIAL CONTACT FORM ________________________________ -->
-
-
-			<form action="" method="post" class="agile_form">
-			<h2>Name</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Date of Birthhhhhhhhhhhhhh</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Place of birth</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Address</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Telephone contact number</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Email address</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>How was the service heard about?????????????????</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Name</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Name</h2>
-			<textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
-
-			<form action="" method="post" class="agile_form">
-			<h2>Does the case involve any of the following? Tick all that apply</h2>
-			 <ul class="agile_info_select">
-				 <li><input type="checkbox" name="view" value="Unlawful Detention" id="Unlawful Detention" required>
-						<label for="Unlawful Detention">Unlawful Detention</label>
-				 </li>
-				 <li><input type="checkbox" name="view" value="Deportation" id="Deportation">
-						<label for="Deportation">Deportation</label>
-				 </li>
-
-				 <li><input type="checkbox" name="view" value="Current Homelessness" id="Current Homelessness">
-						<label for="Current Homelessness">Current Homelessness</label>
-				 </li>
-
-				 <!-- _____________________ INITIAL CONTACT FORM ________________________________ -->
-
-
-				 <!-- _____________________ ELIGIBILTY CHECK ________________________________ -->
-
+	  <h3>Please help us to serve you better by taking a couple of minutes. </h3>
 	    <form action="feedback.php" method="post" class="agile_form">
-		  <h2>Does the primary claimant fit into one of the following criteria:</h2>
-			<h3>
-				1. Came to live in the UK permanently
-				before 1 January 1973 as a
-				Commonwealth citizen and has lived
-				in the UK ever since.
-			</h3>
-
-			<br>
-
-			<h3>
-				2. Came to live in the UK permanently
-				before 1 January 1973 as a
-				Commonwealth citizen and their
-				status lapsed because they left the
-				UK for more than two years but are
-				now in the UK lawfully
-			</h3>
-
-			<br>
-
-			<h3>
-				3. A commonwealth citizen with a Right
-				of Abode and was ordinarily resident
-				in the UK on 1 January 1973
-			</h3>
-
-			<br>
-
-			<h3>
-				4. Previously met one of the above and
-				is now a British citizen
-			</h3>
-
-			<br>
-
-			<h3>
-				5. Came to live in the UK before 31
-				December 1988 and have Right of
-				Abode, settled status or is now a
-				British citizen
-			</h3>
-
-			<br>
-
-			<h3>
-				6. Not currently living in the UK but
-				came to live in the UK permanently
-				before 1 January 1973 as a
-				Commonwealth citizen and have a
-				Right of Abode or settled status or is
-				now a British citizen or, their status
-				lapsed because they left the UK for
-				more than two years
-			</h3>
-
-			<br>
-
-			<h3>
-				7. Their parent or grandparent either (i)
-				came to live in the UK permanently
-				before 1 January 1973, or (ii) has a
-				Right of Abode (or did as is now a
-				British Citizen) and was ordinarily
-				resident in the UK on 1 January 1973
-				and they (if they are a grandchild,
-				their parent) was born in the UK or
-				arrived in the UK before the age of 18
-				and has lived here continuously ever
-				since.
-			</h3>
-
+		  <h2>Did you come to the UK before 1988?</h2>
 			 <ul class="agile_info_select">
-				 <li><input type="radio" name="view" value="Yes" id="Yes" required>
+				 <li><input type="radio" name="view" value="Yes" id="Yes" required> 
 				 	  <label for="Yes">Yes</label>
 				      <div class="check w3"></div>
 				 </li>
-				 <li><input type="radio" name="view" value="no" id="no">
+				 <li><input type="radio" name="view" value="no" id="no"> 
 					  <label for="no"> No</label>
 				      <div class="check w3ls"></div>
 				 </li>
-			 </ul>
-
-				 <!-- _____________________ ELIGIBILTY CHECK ________________________________ -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-				 <form action="feedback2.php" method="post" class="agile_form">
+				 
+				 <form action="feedback.php" method="post" class="agile_form">
 		  <h2>Did your parents or grandparents come to the UK before 1973?</h2>
 			 <ul class="agile_info_select">
-				 <li><input type="radio" name="view1" value="Yes1" id="Yes1" required>
+				 <li><input type="radio" name="view1" value="Yes1" id="Yes1" required> 
 				 	  <label for="Yes1">Yes</label>
 				      <div class="check w3"></div>
 				 </li>
-				 <li><input type="radio" name="view1" value="no1" id="no1">
+				 <li><input type="radio" name="view1" value="no1" id="no1"> 
 					  <label for="no1">No</label>
 				      <div class="check w3ls"></div>
 				 </li>
-			 </ul>
-
-
-				 <form action="feedback3.php" method="post" class="agile_form">
-		  <h2>Do you have any other close relatives who came before 197njjjkhkhkjhkhh3 (i.e.Siblings/aunties) AND do you have extensive family in the UK?</h2>
+				 
+				 <form action="feedback.php" method="post" class="agile_form">
+		  <h2>Do you have any other close relatives who came before 1973 (i.e.Siblings/aunties) AND do you have extensive family in the UK?</h2>
 			 <ul class="agile_info_select">
-				 <li><input type="radio" name="view2" value="Yes2" id="Yes2" required>
+				 <li><input type="radio" name="view2" value="Yes2" id="Yes2" required> 
 				 	  <label for="Yes2">Yes</label>
 				      <div class="check w3"></div>
 				 </li>
-				 <li><input type="radio" name="view2" value="no2" id="no2">
+				 <li><input type="radio" name="view2" value="no2" id="no2"> 
 					  <label for="no2">No</label>
-
+				      <div class="check w3ls"></div>
+				 </li>
+				 
+				 
+			<form action="feedback.php" method="post" class="agile_form">
+		  <h2>Came to live in the UK permanently before 1 January 1973 as a Commonwealth citizen and has lived in the UK ever since.</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view3" value="Yes3" id="Yes3" required> 
+				 	  <label for="Yes3">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view3" value="no3" id="no3"> 
+					  <label for="no3">No</label>
+				      <div class="check w3ls"></div>
+				 </li>
+				 
+				 
+		<form action="feedback.php" method="post" class="agile_form">
+		  <h2>Came to live in the UK permanently before 1 January 1973 as a Commonwealth citizen and their status lapsed because they left the UK for more than two years but are now in the UK lawfully</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view4" value="Yes4" id="Yes4" required> 
+				 	  <label for="Yes4">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view4" value="no4" id="no4"> 
+					  <label for="no4">No</label>
 				      <div class="check w3ls"></div>
 				 </li>
 
-			 </ul>
-
-
-
-
-
+		
+		<form action="feedback.php" method="post" class="agile_form">
+		  <h2>A commonwealth citizen with a Right of Abode and was ordinarily resident in the UK on 1 January 1973</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view5" value="Yes5" id="Yes5" required> 
+				 	  <label for="Yes5">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view5" value="no5" id="no5"> 
+					  <label for="no5">No</label>
+				      <div class="check w3ls"></div>
+				 </li>
+				 
+				 
+		<form action="feedback.php" method="post" class="agile_form">
+		  <h2> Previously met one of the above and is now a British citizen</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view6" value="Yes6" id="Yes6" required> 
+				 	  <label for="Yes6">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view6" value="no6" id="no6"> 
+					  <label for="no6">No</label>
+				      <div class="check w3ls"></div>
+				 </li>
+				 
+		
+		<form action="feedback.php" method="post" class="agile_form">
+		  <h2>Came to live in the UK before 31 December 1988 and have Right of Abode, settled status or is now a British citizen</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view7" value="Yes7" id="Yes7" required> 
+				 	  <label for="Yes7">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view7" value="no7" id="no7"> 
+					  <label for="no7">No</label>
+				      <div class="check w3ls"></div>
+				 </li>
+				 
+				 
+		<form action="feedback.php" method="post" class="agile_form">
+		  <h2>Not currently living in the UK but came to live in the UK permanently before 1 January 1973 as a Commonwealth citizen and have a Right of Abode or settled status or is
+			now a British citizen or, their status lapsed because they left the UK for more than two years</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view8" value="Yes8" id="Yes8" required> 
+				 	  <label for="Yes8">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view8" value="no8" id="no8"> 
+					  <label for="no8">No</label>
+				      <div class="check w3ls"></div>
+				 </li>	 
+				 
+		<form action="feedback.php" method="post" class="agile_form">
+		  <h2>Their parent or grandparent either (i) came to live in the UK permanently before 1 January 1973, or (ii) has a Right of Abode (or did as is now a British Citizen) and was ordinarily
+				resident in the UK on 1 January 1973 and they (if they are a grandchild, their parent) was born in the UK or arrived in the UK before the age of 18 and has lived here continuously ever
+				since.</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view9" value="Yes9" id="Yes9" required> 
+				 	  <label for="Yes9">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view9" value="no9" id="no9"> 
+					  <label for="no9">No</label>
+				      <div class="check w3ls"></div>
+				 </li>
+		
+				 
+			 </ul>	  
 			<h2>If you have specific requirements, please write to us here...</h2>
 			<textarea placeholder="Additional comments" class="w3l_summary" name="comments" required=""></textarea>
 			<input type="text" placeholder="Your Name" name="name"  />
@@ -224,7 +157,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/-->
 	  </form>
 	</div>
 	<div class="agileits_copyright text-center">
-			<p>© Group 4 - Windrush Justice Clinic</p>
+			<p>© Group 4 - Windrush Application</p>
 	</div>
 </body>
 </html>
+
