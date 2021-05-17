4d8572b4def6cd1f5fa6bd97ae2827a71861b775
raimaaa <47975782+raimaaa@users.noreply.github.com>
2021-05-17 19:08:19 +0100

    index2 created with full questions

--- feedback/config.php
+++ feedback/config.php
@@ -1,11 +1,11 @@
 <?php
-ob_start(); //Turns on output buffering 
+ob_start(); //Turns on output buffering
 
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("127.0.0.1", "root", "Rezikuna13", "feedback"); //Connection variable
+$con = mysqli_connect("127.0.0.1", "root", "", "feedback"); //Connection variable
 
-if(mysqli_connect_errno()) 
+if(mysqli_connect_errno())
 {
 	echo "Failed to connect: " . mysqli_connect_errno();
 }
---
+++ feedback/index2.php
@@ -0,0 +1,466 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>FeedbacK Engine</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Elegant Feedback Form  Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //custom-theme -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+</head>
+<body class="agileits_w3layouts">
+    <h1 class="agile_head text-center">Windrush Justice Clinic</h1>
+    <div class="w3layouts_main wrap">
+
+
+
+
+
+	    <form action="feedback.php" method="post" class="agile_form">
+		  <h2>Did you come to the UK before 1988?</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view" value="Yes" id="Yes" required>
+				 	  <label for="Yes">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view" value="no" id="no">
+					  <label for="no"> No</label>
+				      <div class="check w3ls"></div>
+				 </li>
+
+				 <form action="feedback.php" method="post" class="agile_form">
+				 <h2>Did you come to the UK before 1988?</h2>
+					<ul class="agile_info_select">
+						<li><input type="radio" name="view" value="Yes" id="Yes" required>
+							 <label for="Yes">Yes</label>
+								 <div class="check w3"></div>
+						</li>
+						<li><input type="radio" name="view" value="no" id="no">
+							 <label for="no"> No</label>
+								 <div class="check w3ls"></div>
+						</li>
+
+
+						<!-- _____________________ INITIAL CONTACT FORM ________________________________ -->
+
+
+			 <form action="feedback.php" method="post" class="agile_form">
+		  	<h2>Name</h2>
+				 	 <ul class="agile_info_select">
+					 <textarea placeholder="Name" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+			 </form>
+
+			 <form action="feedback.php" method="post" class="agile_form">
+				<h2>Date of Birth</h2>
+					 <ul class="agile_info_select">
+					 <textarea placeholder="Date of Birth" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+			 </form>
+
+			 <form action="feedback.php" method="post" class="agile_form">
+				<h2>Address</h2>
+					 <ul class="agile_info_select">
+					 <textarea placeholder="Address" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+			 </form>
+
+			 <form action="feedback.php" method="post" class="agile_form">
+				<h2>Telephone Contact Number</h2>
+					 <ul class="agile_info_select">
+					 <textarea placeholder="Telephone Contact Number" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+			 </form>
+
+			 <form action="feedback.php" method="post" class="agile_form">
+				<h2>Email Address</h2>
+					 <ul class="agile_info_select">
+					 <textarea placeholder="Email Address" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+			 </form>
+
+			 <form action="feedback.php" method="post" class="agile_form">
+				<h2>How was the service heard about?</h2>
+					 <ul class="agile_info_select">
+					 <textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+			 </form>
+
+			 <form action="feedback.php" method="post" class="agile_form">
+				<h2>Is this a new compensation claim or are you seeking advice and assistance with a review of an offer? </h2>
+					<h2>If you are seeking help with a review, please state when you received the offer as there is a 2 month deadline to accept or decline an offer. </h2>
+					 <ul class="agile_info_select">
+					 <textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+			 </form>
+
+			 <form action="feedback.php" method="post" class="agile_form">
+				<h2>Does the case involve any of the following? Tick all that apply</h2>
+					 <ul class="agile_info_select">
+						 <li><input type="checkbox" name="view" value="Unlawful Detention" id="Unlawful Detention" required>
+				 			 <label for="Unlawful Detention">Unlawful Detention</label>
+				 		</li>
+				 		<li><input type="checkbox" name="view" value="Deportation" id="Deportation">
+				 			 <label for="Deportation">Deportation</label>
+				 		</li>
+
+				 		<li><input type="checkbox" name="view" value="Current Homelessness" id="Current Homelessness">
+				 			 <label for="Current Homelessness">Current Homelessness</label>
+				 		</li>
+					</ul>
+			 </form>
+
+			 <!-- _____________________ INITIAL CONTACT FORM ________________________________ -->
+
+
+
+			 <!-- _____________________ ELIGIBILTY CHECK ________________________________ -->
+
+				<form action="feedback.php" method="post" class="agile_form">
+				<h2>Does the primary claimant fit into one of the following criteria:</h2>
+				<h3>
+				1. Came to live in the UK permanently
+				before 1 January 1973 as a
+				Commonwealth citizen and has lived
+				in the UK ever since.
+				</h3>
+
+				<br>
+
+				<h3>
+				2. Came to live in the UK permanently
+				before 1 January 1973 as a
+				Commonwealth citizen and their
+				status lapsed because they left the
+				UK for more than two years but are
+				now in the UK lawfully
+				</h3>
+
+				<br>
+
+				<h3>
+				3. A commonwealth citizen with a Right
+				of Abode and was ordinarily resident
+				in the UK on 1 January 1973
+				</h3>
+
+				<br>
+
+				<h3>
+				4. Previously met one of the above and
+				is now a British citizen
+				</h3>
+
+				<br>
+
+				<h3>
+				5. Came to live in the UK before 31
+				December 1988 and have Right of
+				Abode, settled status or is now a
+				British citizen
+				</h3>
+
+				<br>
+
+				<h3>
+				6. Not currently living in the UK but
+				came to live in the UK permanently
+				before 1 January 1973 as a
+				Commonwealth citizen and have a
+				Right of Abode or settled status or is
+				now a British citizen or, their status
+				lapsed because they left the UK for
+				more than two years
+				</h3>
+
+				<br>
+
+				<h3>
+				7. Their parent or grandparent either (i)
+				came to live in the UK permanently
+				before 1 January 1973, or (ii) has a
+				Right of Abode (or did as is now a
+				British Citizen) and was ordinarily
+				resident in the UK on 1 January 1973
+				and they (if they are a grandchild,
+				their parent) was born in the UK or
+				arrived in the UK before the age of 18
+				and has lived here continuously ever
+				since.
+				</h3>
+
+				<ul class="agile_info_select">
+				 <li><input type="radio" name="view" value="Yes" id="Yes" required>
+						<label for="Yes">Yes</label>
+							<div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view" value="no" id="no">
+						<label for="no"> No</label>
+							<div class="check w3ls"></div>
+				 </li>
+				</ul>
+
+				 <!-- _____________________ ELIGIBILTY CHECK ________________________________ -->
+
+				 <!-- _____________________ CLOSE FAMILY MEMBER ________________________________ -->
+
+				 <form action="feedback.php" method="post" class="agile_form">
+					<h2>Details of the primary claimant that they are
+							related to and the primary claimants WCS
+							reference number (if known)
+					</h2>
+					<h2>Please ask for the primary claimant's name,
+							DOB, country of birth.
+					</h2>
+						 <ul class="agile_info_select">
+						 <textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						 </li>
+				 </form>
+
+				 <form action="feedback.php" method="post" class="agile_form">
+					<h2>What categories does the client wish to claim
+							under
+					</h2>
+					<h2>Please note that the categories of claim for
+							close family members are not the same for
+							primary claimants
+					</h2>
+						 <ul class="agile_info_select">
+							 <li><input type="checkbox" name="view" value="Impact on life" id="Impact on life" required>
+								 <label for="Impact on life">Impact on life</label>
+							</li>
+							<li><input type="checkbox" name="view" value="Immigration fees and legal fees" id="Immigration fees and legal fees">
+								 <label for="Immigration fees and legal fees">Immigration fees and legal fees</label>
+							</li>
+
+							<li><input type="checkbox" name="view" value="Discretionary" id="Discretionary">
+								 <label for="Discretionary">Discretionary</label>
+							</li>
+						</ul>
+				 </form>
+
+				 <form action="feedback.php" method="post" class="agile_form">
+					<h2>Please take details of the clients claim. Please
+							include when the primary claimant first had
+							difficulties establishing their lawful status and
+							when the client was first made aware.
+					</h2>
+					<h2>Please take details in chronological order.
+					</h2>
+						 <ul class="agile_info_select">
+						 <textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						 </li>
+				 </form>
+
+				 <!-- _____________________ CLOSE FAMILY MEMBER ________________________________ -->
+
+				 <!-- _____________________ REPRESENTATION OF AN ESTATE ________________________________ -->
+
+				<form action="feedback.php" method="post" class="agile_form">
+					<h2>Has the client instructed a solicitor in relation  to their matters under the Windrush
+							Compensation Scheme?
+					</h2>
+					<h2>If so please advise, “the Solicitors rules say that  you can only instruct one solicitor
+						at a time.  Therefore, it may not be in the client's best  interest to have advice from 2
+						different  parties.”
+					</h2>
+					 <ul class="agile_info_select">
+						<li><input type="radio" name="view" value="Yes" id="Yes" required>
+							 <label for="Yes">Yes</label>
+								 <div class="check w3"></div>
+						</li>
+						<li><input type="radio" name="view" value="no" id="no">
+							 <label for="no"> No</label>
+								 <div class="check w3ls"></div>
+						</li>
+					 </ul>
+					 <ul class="agile_info_select">
+					 	<textarea placeholder="Details of solicitor/organisation instructed:" class="w3l_summary" name="comments" required=""></textarea>
+					 </li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>Please take details of the person that whose  estate they represent</h2>
+				 <h2>Please include name, DOB, country of birth date  they passed away</h2>
+						<ul class="agile_info_select">
+						<textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						</li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>What date did the deceased person arrive in  the UK?</h2>
+						<ul class="agile_info_select">
+						<textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						</li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>On what documentation did they travel to the  UK?</h2>
+						<ul class="agile_info_select">
+						<textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						</li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>What date did the deceased person’s parents  arrive in the UK? </h2>
+						<ul class="agile_info_select">
+						<textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						</li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>When did the deceased person first become aware of issues with their status in the UK?</h2>
+				 <h2>Please note what happened for them to become aware of such issues, i.e. where they dismissed
+					 	 from work, unable to claim benefits etc.
+				 </h2>
+						<ul class="agile_info_select">
+						<textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						</li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>Please take details of the issues the deceased  person faced due to difficulties
+					   establishing their lawful status in the UK.
+				 </h2>
+				 <h2>Please take details in chronological order.
+				 </h2>
+						<ul class="agile_info_select">
+						<textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						</li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>Has the client or the deceased person been in  contact with the Home Office since April 2018? </h2>
+				 <h2>If yes, please ask the client if they have a  reference number.</h2>
+						<ul class="agile_info_select">
+						<textarea placeholder="" class="w3l_summary" name="comments" required=""></textarea>
+						</li>
+				</form>
+
+				<form action="feedback.php" method="post" class="agile_form">
+				 <h2>What categories of claim does the client wish to make a claim under.</h2>
+				 <h2>Please tick relevant box(es)</h2>
+						<ul class="agile_info_select">
+							<li><input type="checkbox" name="view" value="Impact on life" id="Impact on life" required>
+								<label for="Impact on life">Impact on life</label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Detention, deportation, removal and return " id="Detention, deportation, removal and return " required>
+							 <label for="Detention, deportation, removal and return e">Detention, deportation, removal and return </label>
+						</li>
+						<li><input type="checkbox" name="view" value="Immigration fees and legal fees" id="Immigration fees and legal fees">
+							 <label for="Immigration fees and legal fees">Immigration fees and legal fees</label>
+						</li>
+						 <li><input type="checkbox" name="view" value="Loss of access to employment " id="Loss of access to employment ">
+								<label for="Loss of access to employment ">Loss of access to employment </label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Loss of access to Child Benefit, Child Tax Credit  or Working Tax Credit" id="Loss of access to Child Benefit, Child Tax Credit  or Working Tax Credit">
+								<label for="Loss of access to Child Benefit, Child Tax Credit  or Working Tax Credit ">Loss of access to Child Benefit, Child Tax Credit  or Working Tax Credit</label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Loss of access to other benefits" id="Loss of access to other benefits">
+								<label for="Loss of access to other benefits ">Loss of access to other benefits </label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Loss of access or refusal to housing " id="Loss of access or refusal to housing ">
+								<label for="Loss of access or refusal to housing ">Loss of access or refusal to housing </label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Homelessness" id="Homelessness">
+								<label for="Homelessness">Homelessness</label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Loss of access to NHS healthcare" id="Loss of access to NHS healthcare">
+								<label for="Loss of access to NHS healthcare">Loss of access to NHS healthcare</label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Loss of access to higher education or paid  overseas tuition fee" id="Loss of access to higher education or paid  overseas tuition fee">
+								<label for="Loss of access to higher education or paid  overseas tuition fee">Loss of access to higher education or paid  overseas tuition fee</label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Loss of access to banking services" id="Loss of access to banking services">
+								<label for="Loss of access to banking services">Loss of access to banking services</label>
+						 </li>
+						 <li><input type="checkbox" name="view" value="Loss of access to driving licence" id="Loss of access to driving licence">
+								<label for="Loss of access to driving licence">Loss of access to driving licence</label>
+						 </li>
+
+
+				 <!-- _____________________ REPRESENTATION OF AN ESTATE ________________________________ -->
+
+				 <!-- _____________________ REFERRAL ________________________________ -->
+
+				 <form action="feedback.php" method="post" class="agile_form">
+				 <h2>CONFLICT CHECK CARRIED OUT</h2>
+					<ul class="agile_info_select">
+						<li><input type="radio" name="view" value="Yes" id="Yes" required>
+							 <label for="Yes">Yes</label>
+								 <div class="check w3"></div>
+						</li>
+						<li><input type="radio" name="view" value="no" id="no">
+							 <label for="no"> No</label>
+								 <div class="check w3ls"></div>
+						</li>
+
+						<form action="feedback.php" method="post" class="agile_form">
+	 				 <h2>HAS A REFERRAL BEEN MADE?</h2>
+	 					<ul class="agile_info_select">
+	 						<li><input type="radio" name="view" value="Yes" id="Yes" required>
+	 							 <label for="Yes">Yes</label>
+	 								 <div class="check w3"></div>
+	 						</li>
+	 						<li><input type="radio" name="view" value="no" id="no">
+	 							 <label for="no"> No</label>
+	 								 <div class="check w3ls"></div>
+	 						</li>
+							<ul class="agile_info_select">
+							<textarea placeholder="IF NO, WHY?" class="w3l_summary" name="comments" required=""></textarea>
+							</li>
+
+							<form action="feedback.php" method="post" class="agile_form">
+							 <h2>DETAILS OF ORGANISATION REFERRED TO</h2>
+									<ul class="agile_info_select">
+									<textarea placeholder="Date of Birth" class="w3l_summary" name="comments" required=""></textarea>
+									</li>
+							</form>
+
+				 <!-- _____________________ REFERRAL ________________________________ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+				 <form action="feedback.php" method="post" class="agile_form">
+		  <h2>Do you have any other close relatives who came before 1973 (i.e.Siblings/aunties) AND do you have extensive family in the UK?</h2>
+			 <ul class="agile_info_select">
+				 <li><input type="radio" name="view2" value="Yes2" id="Yes2" required>
+				 	  <label for="Yes2">Yes</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view2" value="no2" id="no2">
+					  <label for="no2">No</label>
+				      <div class="check w3ls"></div>
+				 </li>
+
+			 </ul>
+		 </form>
+
+			<h2>If you have specific requirements, please write to us here...</h2>
+			<textarea placeholder="Additional comments" class="w3l_summary" name="comments" required=""></textarea>
+			<center><input type="submit" value="submit" class="agileinfo" /></center>
+	  </form>
+	</div>
+	<div class="agileits_copyright text-center">
+			<p>© Group 4 - Windrush Justice Clinic</p>
+	</div>
+</body>
+</html>
